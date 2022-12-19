a1f11e40d7f48c16e101e7fc68364170df455b90
oauch <49686619+oauch@users.noreply.github.com>
2022-12-19 09:50:17 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -6,10 +6,14 @@ for i in a:
     if i == 0 or i == 1:
         continue
     else:
+        # 소수 판별 Boolean
         sosu = True
+        # 소수 비교를 위한 for문
         for j in range(2, i):
+            # 0으로 나뉘어 떨어지면 소수가 아님
             if i % j == 0:
-                sosu = False
+                sosu = False    # False를 주어 밑에 if문 못들어가서 카운트 안됨
+        # 소수이면 카운트
         if sosu == True:
             count += 1    
 
