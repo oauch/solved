bbd982df0125ff896e3c1d609859fb71f6ad5f1b
oauch <49686619+oauch@users.noreply.github.com>
2022-12-21 17:49:16 +0900

    [Bronze III] Title: 배수 찾기, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+while 1:
+    a = int(input())
+    if a == 0:
+        break
+    if a % n == 0:
+        print('%d is a multiple of %d.'%(a, n))
+    else:
+        print('%d is NOT a multiple of %d.'%(a, n))
