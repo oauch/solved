4469e8396e748166e3924d9eb8cb0fd5138f1900
oauch <49686619+oauch@users.noreply.github.com>
2022-12-21 17:06:14 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+n_list = []
+
+for i in range(1, n+1):
+    if n % i == 0:
+        n_list.append(i)
+
+if len(n_list) < k:
+    print(0)
+else:
+    print(n_list[k-1])
