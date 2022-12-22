ef9113fc9d3335b32c5ee0abd67a470114f64efa
oauch <49686619+oauch@users.noreply.github.com>
2022-12-22 11:20:30 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.py
@@ -0,0 +1,10 @@
+result_list = []
+result = 0
+
+for i in range(10):
+    a, b = map(int, input().split())
+    result += b
+    result -= a
+    result_list.append(result)
+
+print(max(result_list))
