798456f3bf1242f4013f09cd9ce9faa0d1e77c57
oauch <49686619+oauch@users.noreply.github.com>
2022-12-26 17:09:52 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,26 @@
+a = input()
+count = 0
+
+for i in a:
+    if i in ' ':
+        count += 2
+    elif i in 'ABC':
+        count += 3
+    elif i in 'DEF':
+        count += 4
+    elif i in 'GHI':
+        count += 5
+    elif i in 'JKL':
+        count += 6
+    elif i in 'MNO':
+        count += 7
+    elif i in 'PQRS':
+        count += 8
+    elif i in 'TUV':
+        count += 9
+    elif i in 'WXYZ':
+        count += 10
+    else:
+        count += 11
+        
+print(count)
