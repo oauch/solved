8a16a0bbfabeeacd4252ea55a84b7522698c8072
oauch <49686619+oauch@users.noreply.github.com>
2022-12-29 10:32:01 +0900

    [Silver IV] Title: 제로, Time: 88 ms, Memory: 31240 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,21 @@
+import sys
+
+K = int(sys.stdin.readline())
+stack = []
+result = 0
+
+for _ in range(K):
+    A = int(sys.stdin.readline())
+
+    # 0인 경우, 가장 최근의 쓴 수 삭제
+    if A == 0:
+        stack.pop()
+    # 그렇지 않으면, 스택에 추가
+    else:
+        stack.append(A)
+
+# 스택에 남아 있는 수들 result에 더하기
+for i in stack:
+    result += i
+
+print(result)
