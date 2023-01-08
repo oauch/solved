a4183d4bb4c8c520a52dfb97d64255493671fa65
oauch <49686619+oauch@users.noreply.github.com>
2023-01-08 14:34:40 +0900

    [Bronze III] Title: Baseball, Time: 72 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10214. Baseball/Baseball.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for _ in range(T):
+    Yonsei = 0
+    Korea = 0
+    for _ in range(9):
+        Y, K = map(int, input().split())
+        Yonsei += Y
+        Korea += K
+
+    if Yonsei > Korea:
+        print('Yonsei')
+    elif Yonsei < Korea:
+        print('Korea')
+    else:
+        print("Draw")
