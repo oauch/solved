19511a9b27a0dc63d6f8fdfe5507519b91f12a80
oauch <49686619+oauch@users.noreply.github.com>
2023-01-13 14:04:23 +0900

    [Bronze III] Title: 개표, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.py
@@ -0,0 +1,19 @@
+V = int(input())
+
+vote = input()
+
+a_count = 0
+b_count = 0
+
+for i in vote:
+    if i == 'A':
+        a_count += 1
+    else:
+        b_count += 1
+
+if a_count > b_count:
+    print('A')
+elif a_count < b_count:
+    print('B')
+else:
+    print('Tie')
